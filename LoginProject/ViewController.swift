de88f38503d34da2ebdbb6f4705e398f40e993ac
Veronika Turiničová <veronikaturinicova@turinicova.kolej.mff.cuni.cz>
2016-11-13 09:16:46 +0100

    Initial Commit

---
+++ LoginProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginProject
+//
+//  Created by Veronika Turiničová on 13.11.16.
+//  Copyright © 2016 Test comp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
